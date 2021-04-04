50000fbe1c715e784ed61927bed59188306af9ba
thelethalcode <kousshikraj.raj@gmail.com>
2021-04-04 17:12:51 +0530

    Basic Stuff

---
+++ p2p/network.py
@@ -0,0 +1,9 @@
+APP_PORT = 7850
+MY_IP = '127.0.0.1'
+
+def send_message(type, **kwargs):
+    pass
+
+
+def recv_message(sock):
+    pass
--- p2p/node.py
+++ p2p/node.py
@@ -1,5 +1,39 @@
+import socket
+import network
 from routingTable import routingTable
 from fileSystem import fileSystem
 
 class node(object):
-    pass
+    
+    def __init__(self):
+        self.GUID = None
+        self.routTab = routingTable()
+        self.fileSys = fileSystem()
+        self.isJoined = False
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind('', network.APP_PORT)
+        self.sock.listen(20)
+
+
+    def joinNetwork(self, bootstrapIP):
+        network.send_message('JOIN', sender=network.MY_IP, dest=bootstrapIP)
+
+        while not self.isJoined:
+            clientsock, address = self.sock.accept()
+            if address[0] != bootstrapIP:
+                continue
+
+            data = network.read_message(clientsock)
+            if data['type'] != 'JOIN_ACK':
+                continue
+
+            routTable.initialise(data['routTable'], data['myGUID'])
+            self.GUID = data['GUID']
+            self.isJoined = True
+
+
+    def run(self, bootstrapIP):
+        self.joinNetwork(bootstrapIP)
+        while True:
+
+
