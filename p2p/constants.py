2e68504dbbcd0af3c4b80c1e08dfde08f6163c98
thelethalcode <kousshikraj.raj@gmail.com>
2021-04-05 20:33:48 +0530

    Node.py Basic Outline

---
+++ p2p/constants.py
@@ -0,0 +1,20 @@
+APP_PORT = 7850
+MY_IP = '127.0.0.1'
+
+JOIN = 'Join'
+JOIN_ACK = 'Join Ack'
+PING = 'Ping'
+PONG = 'Pong'
+QUERY = 'Query'
+QUERY_RESP = 'Query Response'
+TRANSFER_REQ = 'Transfer Request'
+TRANSFER_FILE = 'Transfer File'
+
+TYPE = 'Type'
+SEND_IP = 'Sender IP'
+SEND_GUID = 'Sender GUID'
+DEST_IP = 'Destination IP'
+DEST_GUID = 'Destination GUID'
+ROUTING = 'Routing Table'
+CONTENT = 'Data'
+
